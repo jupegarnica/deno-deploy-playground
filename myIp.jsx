fcd11bb4de807b727ebaa305615af16a4e8e452e
Jupegarnica <juan@geekshubs.com>
2021-05-20 17:13:46 +0200

    style my ip

--- myIp.jsx
+++ myIp.jsx
@@ -3,13 +3,43 @@ import {
   h,
   // PathParams,
   // serve,
-} from 'https://deno.land/x/sift@0.3.0/mod.ts';
+} from 'https://deno.land/x/sift@0.3.1/mod.ts';
+
+const Main = ({ children }) => (
+  <main>
+    <style>
+      {`
+      body {
+        margin:0
+      }
+
+      main {
+        background:linear-gradient(
+            135deg,
+            rgb(77, 47, 126) 30%,
+            #51327577 100%
+        );
+        font-size: clamp(20px, 5vmin, 150px);
+        background-color: tomato;
+        color:white;
+        height:100vh;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI',
+          Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans',
+          'Helvetica Neue', sans-serif;
+    }`}
+    </style>
+    {children}
+  </main>
+);
 
 export function myIp(request) {
   const ip = request.headers.get('x-forwarded-for');
-  return jsx(<h1>{ip}</h1>);
+  return jsx(
+    <Main>
+      <h1>{ip || 'unknown ip'}</h1>
+    </Main>,
+  );
 }
-
-// export function myIpPage(request) {
-//   return new Response('hola')
-// }
